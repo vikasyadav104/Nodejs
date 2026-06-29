45766ee8e3a2e67b4379617172f01e445e94fdb2
vikasyadav104 <vikasyadavskb247@gmail.com>
2026-06-29 15:33:10 +0530

    changes

--- authentication/routes/staticRouter.js
+++ authentication/routes/staticRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   if (!req.user) return res.redirect("/login");
+  //_id automatically creates by mongo db
   const allurls = await URL.find({ createdBy: req.user._id });
   return res.render("home", {
     urls: allurls,
